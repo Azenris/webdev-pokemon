6566d876267a957a6b3f389af16c8d3d4af5010c
Azenris <morleyx22@hotmail.com>
2023-09-10 11:25:58 +0100

    Added Moves.

---
+++ src/data/moves.tsx
@@ -0,0 +1,260 @@
+export enum MoveID {
+  Mega_Punch,
+  Razor_Wind,
+  Swords_Dance,
+  Whirlwind,
+  Mega_Kick,
+  Toxic,
+  Horn_Drill,
+  Body_Slam,
+  Take_Down,
+  Double_Edge,
+  Bubble_Beam,
+  Water_Gun,
+  Ice_Beam,
+  Blizzard,
+  Hyper_Beam,
+  Pay_Day,
+  Submission,
+  Counter,
+  Seismic_Toss,
+  Rage,
+  Mega_Drain,
+  Solarbeam,
+  Dragon_Rage,
+  Thudnerbolt,
+  Thunder,
+  Earthquake,
+  Fissure,
+  Dig,
+  Psychic,
+  Teleport,
+  Mimic,
+  Double_Team,
+  Reflect,
+  Bide,
+  Metronome,
+  Self_Destruct,
+  Egg_Bomb,
+  Fire_Blast,
+  Swift,
+  Skull_Bash,
+  Softboiled,
+  Dream_Eater,
+  Sky_Attack,
+  Rest,
+  Thunder_Wave,
+  Psywave,
+  Explosion,
+  Rock_Slide,
+  Tri_Attack,
+  Substitute,
+};
+
+export type Move = {
+  id: MoveID;
+  name: string;
+};
+
+export const moveBD: Move[] = [
+  {
+    id: MoveID.Mega_Punch,
+    name: "Mega Punch",
+  },
+  {
+    id: MoveID.Razor_Wind,
+    name: "Razor Wind",
+  },
+  {
+    id: MoveID.Swords_Dance,
+    name: "Swords Dance",
+  },
+  {
+    id: MoveID.Whirlwind,
+    name: "Whirlwind",
+  },
+  {
+    id: MoveID.Mega_Kick,
+    name: "Mega Kick",
+  },
+  {
+    id: MoveID.Toxic,
+    name: "Toxic",
+  },
+  {
+    id: MoveID.Horn_Drill,
+    name: "Horn Drill",
+  },
+  {
+    id: MoveID.Body_Slam,
+    name: "Body Slam",
+  },
+  {
+    id: MoveID.Take_Down,
+    name: "Take Down",
+  },
+  {
+    id: MoveID.Double_Edge,
+    name: "Double-Edge",
+  },
+  {
+    id: MoveID.Bubble_Beam,
+    name: "Bubble Beam",
+  },
+  {
+    id: MoveID.Water_Gun,
+    name: "Water Gun",
+  },
+  {
+    id: MoveID.Ice_Beam,
+    name: "Ice Beam",
+  },
+  {
+    id: MoveID.Blizzard,
+    name: "Blizzard",
+  },
+  {
+    id: MoveID.Hyper_Beam,
+    name: "Hyper Beam",
+  },
+  {
+    id: MoveID.Pay_Day,
+    name: "Pay Day",
+  },
+  {
+    id: MoveID.Submission,
+    name: "Submission",
+  },
+  {
+    id: MoveID.Counter,
+    name: "Counter",
+  },
+  {
+    id: MoveID.Seismic_Toss,
+    name: "Seismic Toss",
+  },
+  {
+    id: MoveID.Rage,
+    name: "Rage",
+  },
+  {
+    id: MoveID.Mega_Drain,
+    name: "Mega Drain",
+  },
+  {
+    id: MoveID.Solarbeam,
+    name: "Solarbeam",
+  },
+  {
+    id: MoveID.Dragon_Rage,
+    name: "Dragon Rage",
+  },
+  {
+    id: MoveID.Thudnerbolt,
+    name: "Thudnerbolt",
+  },
+  {
+    id: MoveID.Thunder,
+    name: "Thunder",
+  },
+  {
+    id: MoveID.Earthquake,
+    name: "Earthquake",
+  },
+  {
+    id: MoveID.Fissure,
+    name: "Fissure",
+  },
+  {
+    id: MoveID.Dig,
+    name: "Dig",
+  },
+  {
+    id: MoveID.Psychic,
+    name: "Psychic",
+  },
+  {
+    id: MoveID.Teleport,
+    name: "Teleport",
+  },
+  {
+    id: MoveID.Mimic,
+    name: "Mimic",
+  },
+  {
+    id: MoveID.Double_Team,
+    name: "Double Team",
+  },
+  {
+    id: MoveID.Reflect,
+    name: "Reflect",
+  },
+  {
+    id: MoveID.Bide,
+    name: "Bide",
+  },
+  {
+    id: MoveID.Metronome,
+    name: "Metronome",
+  },
+  {
+    id: MoveID.Self_Destruct,
+    name: "Self Destruct",
+  },
+  {
+    id: MoveID.Egg_Bomb,
+    name: "Egg Bomb",
+  },
+  {
+    id: MoveID.Fire_Blast,
+    name: "Fire Blast",
+  },
+  {
+    id: MoveID.Swift,
+    name: "Swift",
+  },
+  {
+    id: MoveID.Skull_Bash,
+    name: "Skull Bash",
+  },
+  {
+    id: MoveID.Softboiled,
+    name: "Softboiled",
+  },
+  {
+    id: MoveID.Dream_Eater,
+    name: "Dream Eater",
+  },
+  {
+    id: MoveID.Sky_Attack,
+    name: "Sky Attack",
+  },
+  {
+    id: MoveID.Rest,
+    name: "Rest",
+  },
+  {
+    id: MoveID.Thunder_Wave,
+    name: "Thunder Wave",
+  },
+  {
+    id: MoveID.Psywave,
+    name: "Psywave",
+  },
+  {
+    id: MoveID.Explosion,
+    name: "Explosion",
+  },
+  {
+    id: MoveID.Rock_Slide,
+    name: "Rock Slide",
+  },
+  {
+    id: MoveID.Tri_Attack,
+    name: "Tri Attack",
+  },
+  {
+    id: MoveID.Substitute,
+    name: "Substitute",
+  },
+];
