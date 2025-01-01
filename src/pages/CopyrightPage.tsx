1d35e1e90668b5b327d0eac6c08dfa813d1fdbb6
Azenris <morleyx22@hotmail.com>
2025-01-01 00:55:46 +0000

    Update year.

--- src/pages/CopyrightPage.tsx
+++ src/pages/CopyrightPage.tsx
@@ -9,7 +9,7 @@ export function CopyrightPage() {
         <h1>Copyright</h1>
         <p className="about-description">This is a fan website of the original Gameboy Pokémon games.</p>
         <div className="copyright">
-          Pokémon © 2002-2024 Pokémon. © 1995-2024 Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.
+          Pokémon © 2002-2025 Pokémon. © 1995-2025 Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.
         </div>
       </div>
       <Footer />
